3021f6ade111962b3957ee0ebf99204a79b1aa01
Tim Harrold <timjharrold@gmail.com>
2022-03-03 15:41:54 -0500

    Mountain Interactivity

--- src/Snow.jsx
+++ src/Snow.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Row, Container } from 'react-bootstrap';
 import './App.css';
 //import { Link } from 'react-router-dom';
 import * as d3 from "d3";
 import $ from 'jquery';
+import { color } from 'd3';
 
 // var body = document.body,
 //     html = document.documentElement;
@@ -22,9 +23,29 @@ import $ from 'jquery';
 // #302a31
 // #647443
 // #2a4b7e
-const colors = ["#9ebcda", "#357360", "#CABDAF"
-, "#ececd9", "#4c8a63", "#1f5d77", "#302a31", "#647443" ,"#2a4b7e"]
-	
+const colors = ["#9ebcda", "#357360", "#CABDAF", "#ececd9", "#4c8a63", "#1f5d77", "#302a31", "#647443" ,"#2a4b7e"];
+// Light blue #9ebcda
+// sage green #357360
+// tan #CABDAF
+// yellow-ish-tan #ececd9
+// green #4c8a63
+// deeper blue #1f5d77
+// plum #302a31
+// green #647443
+// deep blue #2a4b7e
+const colorPairs = [
+	//["#2a4b7e", "#9ebcda"],
+	["#ececd9", "#CABDAF"],
+	["#4c8a63", "#357360"],
+	["#302a31", "#647443"]
+]
+// sage green 
+
+// green 
+// deeper blue #1f5d77
+// plum 
+// green 
+
 const minMtn = 1;
 const maxMtn = 8;
 
@@ -37,7 +58,6 @@ var timer;
 var margin = {top: 20, right: 20, bottom: 20, left: 20},
 width = window.innerWidth*(10/12),
 height = window.outerHeight;
-// height = Math.max( body.scrollHeight, body.offsetHeight, 
 
 
 let hData = [{cX: 0, cY: 70}]; // Horizontal front-facing line
@@ -53,6 +73,7 @@ function Snow(props) {
 
 	const [sliderVal, setSliderVal] = useState(3);
 	const [rain, setRain] = useState(true);
+	const [day, setDay] = useState(true);
 
 	function handleSlider(event) {
 		setSliderVal(parseInt(event.target.value)+1);
@@ -73,23 +94,6 @@ function Snow(props) {
 		let randY = (Math.random()*60);
 		mData.push({cX: j, cY: randY+j+(width/2)})
 	}
-
-
-	// let x = d3.scaleLinear()
-	// .range([0,width+margin.left+margin.right])
-	// .domain([0,width]);
-
-	// let y = d3.scaleLinear()
-	// .range([height,0])
-	// .domain([0,height]);
-
-	// let mX = d3.scaleLinear()
-	// .range([0,width+margin.left+margin.right])
-	// .domain([0,width/2]);
-
-	// let mY = d3.scaleLinear()
-	// .range([height/2,height])
-	// .domain([0,width]);
 
 
 
@@ -121,6 +125,8 @@ function Snow(props) {
 		.attr("stop-color", "lightgreen")
 		.attr("offset", "1")
 
+	gradient.attr("gradientTransform", "rotate(90)");
+		
 
 
 	var pGrad = svg.append("defs")
@@ -140,7 +146,10 @@ function Snow(props) {
 		.attr("stop-color", "#EE3F6B")
 		.attr("offset", "1")
 
+
+
 	svg.append("rect")
+		.attr("id", "background")
 	    .attr("width", "100%")
 	    .attr("height", "100%")
 	    .attr("fill", "url(#bg-gradient)");
@@ -151,6 +160,8 @@ function Snow(props) {
 	.attr("id", "anim")
 	
 
+	svg.append("g")
+	.attr("id", "mtns")
 
 	// // Forefront hill area
 	// svg.append("path")
@@ -237,136 +248,13 @@ function Snow(props) {
 	}
 	useEffect(() => {
 
-		let svg = d3.select(".main");
+		//let svg = d3.select(".main");
 
-		svg.selectAll("path").remove();
+		//svg.selectAll("path").remove();
 
+	
+		resetMountains();
 
-		let x = d3.scaleLinear()
-		.range([0,width+margin.left+margin.right])
-		.domain([0,width]);
-	
-		let y = d3.scaleLinear()
-		.domain([0,height])
-		.range([height,0]);
-	
-		let mtns = svg.append("g")
-		.attr("id", "mtns");
-	
-		let negative = 1;
-	
-	
-	
-			//for(var j=1;j<=maxMtn;j++) {
-			for(var j=maxMtn; j>=0;j--) {
-			
-				let grad = mtns.append("defs")
-				.append("linearGradient")
-				.attr("id", "grad#"+j)
-				.attr("x1", "0")
-				.attr("x2", "1")
-				.attr("y1", "0")
-				.attr("y2", "1.5")
-	
-				let randClr1 = parseInt(Math.random()*colors.length);
-				let randClr2 = parseInt(Math.random()*colors.length);
-	
-				// Define gradient starts and stops
-				grad.append("stop")
-					.attr("stop-color", colors[randClr1])
-					.attr("offset", "0")
-	
-				grad.append("stop")
-					.attr("stop-color", () => randClr1 === 0 ? colors[5] : colors[randClr2])
-					.attr("offset", "1")
-	
-				// Define gradient starts and stops
-				// grad.append("stop")
-				// 	.attr("stop-color", colors[2])
-				// 	.attr("offset", "0")
-	
-				// grad.append("stop")
-				// 	.attr("stop-color", colors[6])
-				// 	.attr("offset", "1")
-	
-	
-	
-	
-				//let randW = (Math.random()*(width/2))+(width/2);
-	
-				let slope = (Math.random()*1.5)+0.2;
-	
-				let heightVar = (height/(maxMtn))*(maxMtn-j);
-				let variate = ((Math.random()+0.1)*variation);
-				let data = [];
-	
-				// Add hump to tallest mountain
-				// if(j === maxMtn) {
-				// 	//data.push({cX: 0, cY: 70});
-				// 	data.push({cX: 0, cY: 500});
-				// }
-
-				for(var i=0;i<width;i+=10) {
-					let randY = (Math.random()*variate); // Variation per data point
-					data.push({
-						cX: (negative > 0 ? i : (width-i)),
-						// cY: height-(((slope*i)+randY)+heightVar)
-						cY: height-(((slope*i)+randY)+heightVar)
-						//cY: ((height)-((randY+i)))+(heightVar)
-					})
-				}
-				
-				// mtns.selectAll("path")
-				// .data(data)
-				// .enter()
-				// .append("path")
-				// .attr("id", "mtn" + j)
-				// .attr("d", d3.area()
-				// 	.x(d => x(d.cX))
-				// 	.y1(d => x(d.cY))
-				// 	.y0(height+margin.top+margin.bottom)
-				// 	)
-				// .attr("stroke", "black")
-				// //.attr("fill", "url(#grad#" + j + ")")
-				// .attr("fill", colors[randClr1])
-				// .attr("opacity", 0.8);
-			
-				// mtns
-				// .datum(data)
-				// .enter()
-				// .append("path")
-				// .attr("id", "mtn" + j)
-				// .attr("d", d3.area()
-				// 	.x(d => x(d.cX))
-				// 	.y1(d => x(d.cY+20))
-				// 	.y0(height+margin.top+margin.bottom)
-				// 	)
-				// .attr("stroke", "black")
-				// .attr("fill", "url(#grad#" + j + ")")
-				// //.attr("fill", colors[randClr1])
-				// .attr("opacity", 0.5);
-	
-				mtns
-				.datum(data)
-				.append("path")
-				.attr("id", "mtn" + j)
-				.attr("d", d3.area()
-					.x(d => x(d.cX))
-					.y1(d => y(d.cY))
-					.y0(height+margin.top+margin.bottom)
-					)
-				.attr("fill", "url(#grad#" + j + ")")
-				//.attr("fill", colors[2])
-				.attr("opacity", 1);
-				// 0,5 ice
-				// 6 rock
-	
-	
-	
-				negative *= -1;
-	
-	
-			}
 
 
 
@@ -377,8 +265,6 @@ function Snow(props) {
 
 		setSliderVal(3);
 		
-
-
 		let mtns = d3.select("#mtns");
 
 		mtns.selectAll("*").remove();
@@ -418,7 +304,7 @@ function Snow(props) {
 	
 			let grad = mtns.append("defs")
 			.append("linearGradient")
-			.attr("id", "grad#"+j)
+			.attr("id", "grad"+j)
 			.attr("x1", "0")
 			.attr("x2", "1")
 			.attr("y1", "0")
@@ -427,13 +313,15 @@ function Snow(props) {
 			let randClr1 = parseInt(Math.random()*colors.length);
 			let randClr2 = parseInt(Math.random()*colors.length);
 
+			let randPair = parseInt(Math.random()*colorPairs.length);
+
 			// Define gradient starts and stops
 			grad.append("stop")
-				.attr("stop-color", colors[randClr1])
+				.attr("stop-color", colorPairs[randPair][0]) // colors[randClr1]
 				.attr("offset", "0")
 
 			grad.append("stop")
-				.attr("stop-color", colors[randClr2])
+				.attr("stop-color", colorPairs[randPair][1]) // colors[randClr2]
 				.attr("offset", "1")
 
 
@@ -466,7 +354,7 @@ function Snow(props) {
 				.y0(height+margin.top+margin.bottom)
 				)
 			//.attr("stroke", "black")
-			.attr("fill", "url(#grad#" + j + ")")
+			.attr("fill", "url(#grad" + j + ")")
 			.attr("opacity", j <= (sliderVal) ? 1 : 0);
 			// 0,5 ice
 			// 6 rock
@@ -504,22 +392,89 @@ function Snow(props) {
 
 	}, [rain])
 
+	useEffect(() => {
+
+
+		let grad = d3.select("#bg-gradient");
+
+		grad.selectAll("stop").remove();
+
+
+
+		// var gradient = svg.append("defs")
+		// .append("linearGradient")
+		// .attr("id", "bg-gradient")
+		// .attr("x1", "0")
+		// .attr("x2", "1")
+		// .attr("y1", "0")
+		// .attr("y2", "1.5")
+
+		if(day) {
+
+			grad
+			.attr("gradientTransform", "rotate(-90)");
+
+			grad.append("stop")
+				.attr("stop-color", "#9ebcda")
+				.attr("offset", "0")
+	
+			grad.append("stop")
+				.attr("stop-color", "lightgreen")
+				.attr("offset", "1")
+
+		} else {
+	
+			//grad.attr("y2", "3")
+			grad
+			.attr("gradientTransform", "rotate(-90)");
+
+			grad.append("stop")
+				.attr("stop-color", "#141852") 
+				.attr("offset", "0")
+	
+			// #6B4984
+			// #2B2F77
+			grad.append("stop")
+				.attr("stop-color", "#855988")
+				.attr("offset", "0.4")
+
+		}
+
+		d3.select("#background")
+		.transition()
+		.duration(500)
+		.attr("fill", "url(#bg-gradient)");
+
+
+	}, [day])
+
   return (
 
 
 <>
 <div className="position-absolute bg-light p-2" id="panel">
 	
-
+	<Container>
 	<Form.Label>Mountain Count</Form.Label>
   		<Form.Range className="mx-2" max={maxMtn} min={minMtn} value={sliderVal-1} onChange={(event) => handleSlider(event)} />
 	<Button variant="dark" onClick={() => resetMountains()}>Reset</Button>
-	<Form.Check 
-        type={"checkbox"}
-        id={"rainCheck"}
-        label={"Toggle Rain"}
-		checked={rain}
-		onChange={() => setRain(!rain)} />
+	<Row className="px-3 mx-3">
+		<Form.Check 
+			type={"checkbox"}
+			id={"rainCheck"} // lol
+			label={"Toggle Rain"}
+			checked={rain}
+			onChange={() => setRain(!rain)} />
+		<Form.Check 
+			className="mx-3"
+			type={"checkbox"}
+			id={"nightCheck"}
+			label={"Toggle Day"}
+			checked={day}
+			onChange={() => setDay(!day)} />
+	</Row>
+	</Container>
+
 
 	{/* <Form.Check 
         type={"checkbox"}
